d1d05f51ab061f63d87b60d1849a6573e4712917
Siluveru Kiran Kumar <siluverukirankumar6@gmail.com>
2025-11-25 13:09:37 +0530

    path and os modules learning

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 import './global_obj.js'
 import log_module from './log_module.js'
+import path_module from './path_module.js'
 
 const sayHello = (name) =>{
     console.log(`Hello ${name}, Have a great Day`)
