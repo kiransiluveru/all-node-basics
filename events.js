b6c873a0524cb002e9c71a5060789600532221ef
Siluveru Kiran Kumar <siluverukirankumar6@gmail.com>
2025-11-27 13:08:16 +0530

    Adder logger class with emit event functionality

--- events.js
+++ events.js
@@ -1,4 +1,5 @@
 import EventEmitter from "events";
+import Logger from "./logger.js";
 const eventEmitter = new EventEmitter();
 
 const triggerer = () => {
@@ -13,3 +14,20 @@ eventEmitter.emit("trigger");
 eventEmitter.off("trigger", triggerer);
 
 eventEmitter.emit("trigger");
+
+const app_logger = new EventEmitter();
+
+app_logger.on("message", (message = "") => {
+  console.log(`Message received: ${message}`);
+});
+
+app_logger.emit("message");
+
+console.log()
+
+const logger = new Logger();
+logger.on("messageLogged", () => {
+  console.log("messageLogged event triggered");
+});
+
+logger.log("Hey I'm from logger");
