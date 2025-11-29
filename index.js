92238da7c63d58542e9e9cf23eaded97a6d6cdcf
Siluveru Kiran Kumar <siluverukirankumar6@gmail.com>
2025-11-29 16:24:49 +0530

    express get and post endpoints created

--- index.js
+++ index.js
@@ -1,18 +1,65 @@
+import express from "express";
+const app = express();
+// Parse JSON request bodies
+app.use(express.json());
+// Parse URL-encoded request bodies
+app.use(express.urlencoded({ extended: true }));
 
-import './global_obj.js'
-import log_module from './log_module.js'
-import path_module from './path_module.js'
+const PORT = 3000;
 
-const sayHello = (name) =>{
-    console.log(`Hello ${name}, Have a great Day`)
-}
+const courses = [
+  { id: 1, name: "React" },
+  { id: 2, name: "Angular" },
+  { id: 3, name: "NodeJS" },
+  { id: 4, name: "Devops" },
+];
 
-sayHello("Kiran")
+app.get("/", (req, res) => {
+  const { params, path, query } = req;
+  console.log("params", params, path, query);
+  res.send({ name: "Kiran" });
+  res.end();
+});
 
-console.log(`index.js >>>>>> global this greet`,globalThis.greet) 
+app.get("/api/courses", (req, res) => {
+  const { params, path, query } = req;
+  console.log("params", params, path, query);
+  res.send(courses);
+});
 
+// 400 bad request
+// 404 resource not found
+// 200 success
+// 201 resource created successfully
 
-// console.log(import);
-console.log(import.meta);
+app.get("/api/courses/:id", (req, res) => {
+  const { params, query } = req;
+  console.log("params", params, query);
+  const course = courses.find((courseObj) => courseObj.id === parseInt(params.id));
+  if (!course) {
+    return res.status(404).send("Course with given ID not found!!!");
+  }
+  res.send({ course, message: "Fetched course details successfully" });
+});
 
-log_module.log("I am from log module imported func")
+app.get("/api/courses/:year/:month", (req, res) => {
+  const { params, path, query } = req;
+  console.log("params", params, path, query);
+  res.send({ Year: params.year, Month: params.month, query: req.query });
+});
+
+app.post("/api/courses", (req, res) => {
+  const { params, path, query, body } = req;
+  console.log("params", body);
+  const newCourse = {
+    id: courses.length + 1,
+    name: body.name,
+  };
+
+  courses.push(newCourse);
+  res.status(201).send({ message: "Course Created Successfully" });
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
